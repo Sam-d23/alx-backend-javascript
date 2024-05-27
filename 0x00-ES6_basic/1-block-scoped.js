d7cda94ec1a332ccb2e7847575a752535b2fbe7f
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-05-27 19:28:49 +0300

    Task 01

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  let newTask = task;
-  let newTask2 = task2;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    newTask = true;
-    newTask2 = false;
+    let newTask = true;
+    let newTask2 = false;
+    return [newTask, newTask2];
   }
 
-  return [newTask, newTask2];
+  return [task, task2];
 }
