67c5f8c6bf5e9fc26aac421b70e462213ddbf790
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-05-30 17:18:25 +0300

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,33 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage'
+        );
+      }
     }
-    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  // Method to check if evacuationWarningMessage is implemented
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-}
-
-// Example of a subclass that extends Building and implements evacuationWarningMessage
-class OfficeBuilding extends Building {
-  constructor(sqft, name) {
-    super(sqft);
-    this._name = name;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  evacuationWarningMessage() {
-    return `Evacuate the ${this.name} building immediately!`;
+  set sqft(value) {
+    this._sqft = value;
   }
 }
