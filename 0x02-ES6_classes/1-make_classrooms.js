94d538e4cbe005f47e4a825db725a00dd9a46793
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-05-30 16:05:47 +0300

    Task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,5 @@
-// Importing ClassRoom class from 0-classroom.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-// Defining initializeRooms function
 export default function initializeRooms() {
-  // Creating ClassRoom instances with the required sizes
-  const first = new ClassRoom(19);
-  const second = new ClassRoom(20);
-  const third = new ClassRoom(34);
-  
-  // Returning the instances in an array
-  return [first, second, third];
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
