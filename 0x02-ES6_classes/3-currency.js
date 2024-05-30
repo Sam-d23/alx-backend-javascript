7bb8fbb9a71ffe74be31d0e309d503dd4f909387
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-05-30 16:32:58 +0300

    Task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,14 +1,26 @@
 export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-    get code() { return this._code; }
-    set code(value) { this._code = value; }
+  get code() {
+    return this._code;
+  }
 
-    get name() { return this._name; }
-    set name(value) { this._name = value; }
+  set code(value) {
+    this._code = value;
+  }
 
-    displayFullCurrency() { return `${this._name} (${this._code})`; }
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
