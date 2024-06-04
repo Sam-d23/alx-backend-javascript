f37edaef1fa43ff159108ff6b5eb4e787ef0e4cc
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-04 22:17:28 +0300

    Task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
+  if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
   const buffer = new DataView(new ArrayBuffer(length), 0, length);
-  buf.setInt8(position, value);
+  buffer.setInt8(position, value);
   return buffer;
 }
