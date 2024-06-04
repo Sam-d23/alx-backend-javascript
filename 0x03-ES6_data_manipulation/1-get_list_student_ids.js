b675e43196f9b3ef78f5fcec5e9ffc99f0e5fbcf
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-04 14:49:22 +0300

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,6 @@
-export default function getListStudentsIds() {
+import getListStudents from "./0-get_list_students.js";
+
+export default function getListStudentsIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
