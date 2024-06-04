9ce8d107a0393e1b43fb0c044a191742bb91c3ad
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-04 23:36:29 +0300

    Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,16 +2,9 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-
-  const updatedMap = new Map();
-
-  for (const [item, quantity] of map) {
-    if (quantity === 1) {
-      updatedMap.set(item, 100);
-    } else {
-      updatedMap.set(item, quantity);
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
     }
-  }
-
-  return updatedMap;
+  });
 }
