be2b46defb737b48275b25d9858310d3510aaf24
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-04 21:49:39 +0300

    task 05

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-	const buffer = new ArrayBuffer(length);
-	const int8Array = new Int8Array(buffer);
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range')
-  }
-  int8Array[position] = value;
-  return buffer;
+	if (position < 0 || position >= length) {
+		throw new Error('Position outside range');
+        }
+	let buffer = new ArrayBuffer(length);
+	let int8Array = new int8Array(buffer);
+	int8Array[position] = value;
+	return buffer;
 }
