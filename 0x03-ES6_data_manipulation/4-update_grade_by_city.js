5edc1d9c1f0ce17460e0797847dbf32ce1be885d
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-04 16:09:30 +0300

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   if (students instanceof Array) {
   return students.filter(student => student.location === city)
                 .map(student => {
-                  const matchedGrade = newGrades.find(grade => grade.studentId === student.id);
+                  const matchedGrade = newGrades.filter(grade => grade.studentId === student.id);
                   return { ...student, grade: matchedGrade ? matchedGrade.grade : 'N/A' };
                 });
   }
