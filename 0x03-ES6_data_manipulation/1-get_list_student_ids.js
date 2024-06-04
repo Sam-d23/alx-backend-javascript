e6f7f5035fc3af4b72041597528dd7ee4e5a0835
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-04 23:50:38 +0300

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,4 @@
-export default function getListStudentIds(students) {
-  // Check if the input is an array
-  if (students instanceof Array) {
-    // Use map to extract and return the ids from the student objects
-    return students.map(student => student.id);
-  }
-  // Return an empty array if the input is not an array
-  return [];
+export default function getListStudentIds(arg) {
+  if (!Array.isArray(arg)) return [];
+  return arg.map((value) => value.id);
 }
