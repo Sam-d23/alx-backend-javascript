ded533edd89465bc53e3d9b596d2bd158e33b37c
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-06 02:20:34 +0300

    Task 09

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,15 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
-export default function getFullBudgetObject(income: number, gdp: number, capita: number) {
+export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income: number) {
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros(income: number) {
+    getIncomeInEuros(income) {
       return `${income} euros`;
-    }
+    },
   };
 
   return fullBudget;
