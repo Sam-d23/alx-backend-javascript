fc6ae191406c9fde198964e57359a4a8526da32a
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-23 09:02:45 +0300

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,10 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-function startProgram() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.exit();
+});
 
-  console.log('Welcome to Holberton School, what is your name?');
-
-  rl.question('', (name) => {
-    console.log(`Your name is: ${name}`);
-    rl.close();
-  });
-
-  rl.on('close', () => {
-    console.log('This important software is now closing');
-    process.exit(0);
-  });
-}
-
-module.exports = startProgram;
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
