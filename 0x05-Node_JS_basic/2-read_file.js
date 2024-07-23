b2b0905ca430d86623fdf8be8f1cb8c3da312d8d
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-23 11:41:20 +0300

    Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
-
+/**
+ * Count students and log details about them.
+ * @param {string} filePath - The path to the CSV file.
+ */
 const countStudents = (filePath) => {
   if (!fs.existsSync(filePath)) {
     throw new Error('Cannot load the database');
@@ -10,9 +13,10 @@ const countStudents = (filePath) => {
   }
   const linesArray = fs
     .readFileSync(filePath, 'utf-8')
-    .toString('utf-8')
     .trim()
-    .split('\n');
+    .split('\n')
+    .filter(Boolean);
+  
   const groups = {};
   const headers = linesArray[0].split(',');
   const propertyNames = headers.slice(0, -1);
@@ -21,18 +25,20 @@ const countStudents = (filePath) => {
     const values = line.split(',');
     const properties = values.slice(0, -1);
     const category = values[values.length - 1];
+
     if (!groups[category]) {
       groups[category] = [];
     }
-    const entries = propertyNames
-      .map((name, index) => [name, properties[index]]);
+
+    const entries = propertyNames.map((name, index) => [name, properties[index]]);
+    
     groups[category].push(Object.fromEntries(entries));
   }
 
-  const totalStudents = Object
-    .values(groups)
-    .reduce((total, group) => total + group.length, 0);
+  const totalStudents = Object.values(groups).reduce((total, group) => total + group.length, 0);
+
   console.log(`Number of students: ${totalStudents}`);
+
   for (const [category, students] of Object.entries(groups)) {
     const names = students.map(student => student.firstname).join(', ');
     console.log(`Number of students in ${category}: ${students.length}. List: ${names}`);
