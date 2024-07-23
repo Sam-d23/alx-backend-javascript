586cf9af18e01baad9d08c0c0aabac31aafe2178
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-23 11:56:21 +0300

    Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,10 +7,10 @@ function countStudents(fileName) {
   try {
     const fileContents = fs.readFileSync(fileName, 'utf-8');
     const lines = fileContents.toString().split('\n');
-    for (let i = 0; i < lines.length; i += 1) {
-      if (lines[i]) {
+    for (let j = 0; j < lines.length; j += 1) {
+      if (lines[j]) {
         length += 1;
-        const field = lines[i].toString().split(',');
+        const field = lines[j].toString().split(',');
         if (Object.prototype.hasOwnProperty.call(students, field[3])) {
           students[field[3]].push(field[0]);
         } else {
