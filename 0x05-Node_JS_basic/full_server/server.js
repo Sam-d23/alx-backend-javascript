907bad0b4a0713cc2e1f7d9a672bf1ffe290b299
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-23 16:33:57 +0300

    Task 8

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import routes from './routes/index.js';
+const express = require('express');
+
+const router = require('./routes/index');
 
 const app = express();
 const port = 1245;
 
-app.use('/', routes);
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+app.listen(port);
 
 export default app;
