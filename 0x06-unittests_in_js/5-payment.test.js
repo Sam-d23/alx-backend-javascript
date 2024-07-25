cf785e24ca80ceb2c6b155fb623b0f6edd5b85a9
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-25 12:17:44 +0300

    Task 6

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -7,15 +7,15 @@ describe("sendPaymentRequestToApi", function() {
     beforeEach("Set up spy to use for each test", function() {
 	sinon.spy(console, "log");
     });
-    afterEach("restore spy after each test", function() {
+    afterEach("restoring spy after each test", function() {
 	console.log.restore();
     });
-    it("check that console.log is called with the right arg", function() {
+    it("checking whether console.log is called with the right arg", function() {
 	sendPaymentRequestToApi(100, 20);
 
 	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
     });
-    it("check that console.log is called with the right arg", function() {
+    it("checking whether console.log is called with the right arg", function() {
 	sendPaymentRequestToApi(10, 10);
 
 	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
