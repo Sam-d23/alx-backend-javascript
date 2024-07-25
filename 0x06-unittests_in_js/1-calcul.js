06124deab52d88c1f959bc42e738ed181ed72a94
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-07-25 10:20:20 +0300

    Task 1

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,23 @@
+function calculateNumber(type, a, b) {
+    const x = Math.round(a);
+    const y = Math.round(b);
+    let c = 0;
+    switch (type) {
+    case 'SUM':
+	c = x + y;
+	break;
+    case 'SUBTRACT':
+	c = x - y;
+	break;
+    case 'DIVIDE':
+      if (y === 0) {
+            c = "Error";
+      } else {
+            c = x / y;
+      }
+      break;
+    }
+    return c;
+}
+
+module.exports = calculateNumber;
